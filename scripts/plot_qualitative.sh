ae90b41b513d663466077980462bb31983fee9c6
Alvin <kimbowa2alvin@gmail.com>
2026-08-26 18:43:02 -0700

    feat: add Dataset219 support across configuration and plotting scripts

--- scripts/plot_qualitative.sh
+++ scripts/plot_qualitative.sh
@@ -21,6 +21,7 @@ datasets=(
     Dataset213_lesion_KE_smi_gfap
     Dataset217_lesion_MY_smi_gfap
     Dataset218_lesion_eric_smi_gfap
+    Dataset219_lesion_MYK_smi_gfap
     # Dataset203_neurites_yvonne_smi_2px_scaleaug
 )
 experiments=(
